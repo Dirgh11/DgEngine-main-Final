2318282b0510c48680a662afa586e6d07788e66f
Dirgh11 <dirghengineer@gmail.com>
2026-05-27 21:00:42 -0700

    Add files via upload
    HelloMeshBuilder

---
+++ DgEngine-main/VGP242/04_HelloMeshBuilder/ShapeState.h
@@ -0,0 +1,27 @@
+#pragma once
+#include"DgEngine/Inc/DgEngine.h"
+
+class ShapeState : public DgEngine::AppState
+{
+public:
+	void Initialize() override;
+	void Terminate() override;
+	void Update(float deltaTime) override;
+	void Render()override;
+
+protected:
+	virtual void CreateShape();
+
+	DgEngine::Graphics::MeshPX mMesh;
+
+	DgEngine::Graphics::Camera mCamera;
+	DgEngine::Graphics::ConstantBuffer mTransformBuffer;
+	DgEngine::Graphics::MeshBuffer mMeshBuffer;
+	DgEngine::Graphics::VertexShader mVertexShader;
+	DgEngine::Graphics::PixelShader mPixelShader;
+
+	// new
+	DgEngine::Graphics::Texture mTexture;
+	DgEngine::Graphics::Sampler mSampler;
+};
+
